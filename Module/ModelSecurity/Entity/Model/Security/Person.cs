a4be35b0c99a83d96f1c6ab29e2f5c2e9b34721b
Cristian Bahamon <Cbahamongranados@gmail.com>
2024-07-26 15:33:59 -0500

    Se sube el moduleSecurity

---
+++ Module/ModelSecurity/Entity/Model/Security/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Model.Security
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+
+        public string First_name { get; set; }
+
+        public string Last_name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Addres { get; set; }
+
+        public string Type_document { get; set; }
+
+        public int Document {  get; set; }
+
+        public int Birth_of_date { get; set; }
+
+
+
+
+
+    }
+}
